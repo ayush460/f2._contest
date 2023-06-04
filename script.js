09367e6b9694656f15e54019d48a4a2914bbb8a3
Ayush <amaddheshiya2003@gmail.com>
2023-06-04 22:23:56 +0530

    second

--- script.js
+++ script.js
@@ -1,80 +1,86 @@
 function getMenu() {
-    return fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
-      .then(response => response.json())
-      .then(data => {
-        const menuElement = document.getElementById('menu');
-        
-        // Display food items to the user
-        data.forEach(item => {
-          const itemElement = document.createElement('div');
-          itemElement.textContent = `${item.name}: $${item.price}`;
-          menuElement.appendChild(itemElement);
-        });
-      })
-      .catch(error => {
-        // Handle any errors that occur during the fetch request
-        console.error('Error:', error);
+  return fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
+    .then(response => response.json())
+    .then(data => {
+      const menuList = document.getElementById('menu-list');
+      data.forEach(item => {
+        const listItem = document.createElement('li');
+        const image = document.createElement('img');
+        image.src = item.imgSrc;
+        image.alt = item.name;
+       
+        image.style.width = '400px';
+        image.style.height = '200px';
+        listItem.innerText = `${item.name} - ${item.price}`;
+        listItem.appendChild(image);
+        menuList.appendChild(listItem);
       });
-  }
-  
-  // Rest of the code remains the same...
-  
-  
-  function takeOrder() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        // Randomly select 3 burgers
-        const burgers = ['Cheeseburger', 'Chicken Burger', 'Veggie Burger'];
-        const randomBurgers = [];
-        for (let i = 0; i < 3; i++) {
-          const randomIndex = Math.floor(Math.random() * burgers.length);
-          randomBurgers.push(burgers[randomIndex]);
-        }
-        
-        const order = { burgers: randomBurgers };
-        resolve(order);
-      }, 2500);
+    })
+    .catch(error => {
+      console.error('Error fetching menu:', error);
     });
+}
+
+function takeOrder() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const burgers = getRandomBurgers(3);
+      const order = {
+        burgers: burgers
+      };
+      resolve(order);
+    }, 2500);
+  });
+}
+
+function orderPrep() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ order_status: true, paid: false });
+    }, 1500);
+  });
+}
+
+function payOrder() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ order_status: true, paid: true });
+    }, 1000);
+  });
+}
+
+function thankyouFnc() {
+  alert('Thank you for eating with us today!');
+}
+
+function getRandomBurgers(count) {
+  const burgers = ['Cheeseburger', 'Burger', 'Veggie Burger', 'Chicken Burger'];
+  const randomBurgers = [];
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * burgers.length);
+    randomBurgers.push(burgers[randomIndex]);
   }
-  
-  function orderPrep() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ order_status: true, paid: false });
-      }, 1500);
+  return randomBurgers;
+}
+
+function startOrder() {
+  getMenu()
+    .then(() => {
+      return takeOrder();
+    })
+    .then(order => {
+      console.log('Order:', order);
+      return orderPrep();
+    })
+    .then(preparation => {
+      console.log('Preparation:', preparation);
+      return payOrder();
+    })
+    .then(payment => {
+      console.log('Payment:', payment);
+      thankyouFnc();
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-  }
-  
-  function payOrder() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ order_status: true, paid: true });
-      }, 1000);
-    });
-  }
-  
-  function thankyouFnc() {
-    alert('Thank you for eating with us today!');
-  }
-  
-  function startOrder() {
-    getMenu()
-      .then(() => takeOrder())
-      .then(order => {
-        console.log('Order:', order);
-        return orderPrep();
-      })
-      .then(orderStatus => {
-        console.log('Order Status:', orderStatus);
-        return payOrder();
-      })
-      .then(paymentStatus => {
-        console.log('Payment Status:', paymentStatus);
-        thankyouFnc();
-      })
-      .catch(error => {
-        // Handle any errors that occur during promise resolutions
-        console.error('Error:', error);
-      });
-  }
-  
+}
